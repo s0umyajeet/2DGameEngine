3b8ce60b0b0f102698061316cec5ff466f295185
Soumyajeet <mandal.soumyajeet@gmail.com>
2020-07-09 21:07:09 +0530

    Created member functions for TextureManager

--- TextureManager.cpp
+++ TextureManager.cpp
@@ -1,1 +1,24 @@
 #include "TextureManager.h"
+
+bool TextureManager::load(std::string id, std::string fileName)
+{
+	return false;
+}
+
+void TextureManager::drop(std::string id)
+{
+}
+
+bool TextureManager::draw(std::string id, SDL_Renderer* _renderer, int x, int y, int width, int height, SDL_RendererFlip flip)
+{
+	return false;
+}
+
+bool TextureManager::draw_frame(std::string id, SDL_Renderer* _renderer, int x, int y, int width, int height, int current_frame, int current_row, SDL_RendererFlip flip)
+{
+	return false;
+}
+
+void TextureManager::clean()
+{
+}
--- TextureManager.h
+++ TextureManager.h
@@ -6,12 +6,17 @@ class TextureManager
 {
 public:
 	static TextureManager& getInstance();
-	bool Load(std::string id, std::string fileName);
+	bool load(std::string id, std::string fileName);    
+	void drop(std::string id);
+	bool draw(std::string id, SDL_Renderer* _renderer, int x, int y, int width, int height, SDL_RendererFlip flip);
+	bool draw_frame(std::string id, SDL_Renderer* _renderer, int x, int y, int width, int height, int current_frame, int current_row, SDL_RendererFlip flip);
+	void clean();
 
 private:
 	TextureManager();
 	~TextureManager();
 	
+
 
 };
 
