20690b10e7e3180ecd66abbf78e84a26e2c668eb
Soumyajeet <mandal.soumyajeet@gmail.com>
2020-07-17 01:35:46 +0530

    Made some cosmetic changes to the code

--- Engine.cpp
+++ Engine.cpp
@@ -29,7 +29,7 @@ SDL_Renderer* Engine::getRenderer()
 bool Engine::init(const char* title, int x_pos, int y_pos, int width, int height, bool fullscreen) {
 	Uint32 flags = 0;
 	time_t curr_time = NULL;
-	std::string image_path = "assets/hills.jpg";
+	std::string image_path = "assets/awesome_face.png";
 
 	if (fullscreen) {
 		flags = SDL_WINDOW_FULLSCREEN;
@@ -50,7 +50,7 @@ bool Engine::init(const char* title, int x_pos, int y_pos, int width, int height
 			if (_renderer = SDL_CreateRenderer(_window, -1, 0)) {	
 				std::cout << "Renderer created successfully... " << std::endl;
 				
-				if (TextureManager::getInstance().load("hills", image_path)) {
+				if (TextureManager::getInstance().load("awesome_face", image_path)) {
 					std::cout << "Image loading successful..." << std::endl;
 				} else {
 					std::cout << "Image loading failed..." << std::endl;
@@ -91,7 +91,7 @@ void Engine::render()
 	//clear the renderer
 	SDL_RenderClear(_renderer);
 	//draw to the screen
-	TextureManager::getInstance().draw("hills", _renderer, 0, 0, 0, 0, SDL_FLIP_NONE);
+	TextureManager::getInstance().draw("awesome_face", _renderer, 0, 0, 0, 0, SDL_FLIP_NONE);
 	//SDL_RenderCopy(_renderer, _texture, &_source_rect, NULL);
 	SDL_RenderPresent(_renderer);
 
