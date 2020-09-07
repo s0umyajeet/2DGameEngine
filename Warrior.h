ddce421117cd421fb772cc7bc23a68f87c3e21e6
Soumyajeet <mandal.soumyajeet@gmail.com>
2020-09-07 06:08:54 +0530

    [wip] | Created a Warrior class, derived from character class

---
+++ Warrior.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Character.h"
+
+class Warrior : public Character
+{
+public:
+	//Constructor
+	Warrior(Properties* props);		
+
+	//I am still keeping these virtual in case I wanna create sub classes of warriors to implement
+	//different types of warriors. 
+	virtual void draw();
+	virtual void update(float dt);
+	virtual void clean();
+	
+public:
+	int m_currentRow;		//Current row in the sprite sheet
+	int m_currentFrame;		//Current frame in the sprite sheet
+
+	int m_rowCount;		//Number of rows in the sprite sheet for this character
+	int m_frameCount;	//number of frames in the sprite sheet for this character
+
+	int m_animationSpeed;	//For controlling frames per second 
+};
+
