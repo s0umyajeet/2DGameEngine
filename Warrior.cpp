26808081e290a54163ff50b13195f3818fe4da82
Soumyajeet <mandal.soumyajeet@gmail.com>
2020-09-09 05:09:03 +0530

    Changed function call TextureManager::draw_frame() inside Warrior::draw() [Since it doesn't take renderer as a parameter]

--- Warrior.cpp
+++ Warrior.cpp
@@ -16,7 +16,7 @@ Warrior::Warrior(Properties* props)
 
 void Warrior::draw()
 {
-	TextureManager::getInstance().draw_frame(m_textureID, Engine::getInstance().getRenderer(), m_transform->m_X, m_transform->m_Y, m_width, m_height, m_currentRow, m_currentFrame, SDL_FLIP_NONE);
+	TextureManager::getInstance().draw_frame(m_textureID, m_transform->m_X, m_transform->m_Y, m_width, m_height, m_currentRow, m_currentFrame, SDL_FLIP_NONE);
 }
 
 void Warrior::update(float dt)
